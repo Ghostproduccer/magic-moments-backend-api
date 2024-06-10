ff7321825141550e2d45573ac38036d622197ef8
Ruben <rdiazmarey@gmail.com>
2024-06-10 14:01:57 +0200

    Actualizado repo y actualizado gitignore con los archivos de configuración para cada perfil (dev y prod de momento)

--- src/main/java/com/magicmoments/backendapi/BackendApiApplication.java
+++ src/main/java/com/magicmoments/backendapi/BackendApiApplication.java
@@ -2,7 +2,6 @@ package com.magicmoments.backendapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class BackendApiApplication {
@@ -10,5 +9,4 @@ public class BackendApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendApiApplication.class, args);
     }
-
 }
